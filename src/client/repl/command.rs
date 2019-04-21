00d63be0ba1cb29a2cb3134f59a2ffa65369a355
David Krentzlin <certainty@users.noreply.github.com>
2019-04-21 22:13:29 +0200

    fix(command parser): Don't accept garbage after valid command (#3)
    This makes the command parser more strict in
    that it requires to match the whole input.
    
    Before this change commands would be accepted
    even if garbage data was still in the input but unconsumed.

--- src/client/repl/command.rs
+++ src/client/repl/command.rs
@@ -13,6 +13,12 @@ pub enum Command {
 }
 
 mod parser {
+    #[derive(Debug, PartialEq)]
+    pub enum Error {
+      ParsingIncomplete,
+      ParseError
+    }
+
     use super::*;
     named!(cmd_quit<CompleteStr, Command>, map!(tag_s!("quit"), |_| Command::Quit));
 
@@ -55,14 +61,17 @@ mod parser {
         cmd: alt_complete!(cmd_quit | cmd_lookup | cmd_delete | cmd_insert | cmd_list_keys | cmd_stats | cmd_help) >> 
         (cmd))
     );
-  }
+}
 
-  pub fn parse(input: &str) -> Result<Command, String> {
-    match parser::parse_command(CompleteStr(input.trim())) {
-      Ok((_, cmd)) => Result::Ok(cmd),
-      _ => Result::Err(String::from("Unknown command"))
-    }
+use parser::Error;
+
+pub fn parse(input: &str) -> Result<Command, Error> {
+  match parser::parse_command(CompleteStr(input.trim())) {
+    Ok((CompleteStr(""), cmd)) => Result::Ok(cmd),
+    Ok(_) => Result::Err(Error::ParsingIncomplete),
+    Err(_) => Result::Err(Error::ParseError)
   }
+}
 
 
 #[cfg(test)]
@@ -78,28 +87,32 @@ mod tests {
   fn parse_insert_succeeds() {
     assert_eq!(
       parse(&String::from(":insert foo bar")), 
-      Ok(Command::Insert(String::from("foo"), String::from("bar"))))
+      Ok(Command::Insert(String::from("foo"), String::from("bar")))
+    )
   }
 
 #[test]
   fn parse_insert_fails_when_key_is_missing() {
     assert_eq!(
       parse(&String::from(":insert")), 
-      Err(String::from("Unknown command")))
+      Err(Error::ParseError)
+    )
   }
 
   #[test]
   fn parse_insert_fails_when_value_is_missing() {
     assert_eq!(
       parse(&String::from(":insert bar")), 
-      Err(String::from("Unknown command")))
+      Err(Error::ParseError)
+    )
   }
 
   #[test]
-  fn parse_insert_ignores_garbage() {
+  fn parse_fails_with_appended_garbage() {
     assert_eq!(
       parse(&String::from(":insert foo bar garbage")),
-      Ok(Command::Insert(String::from("foo"), String::from("bar"))));
+      Err(Error::ParsingIncomplete)
+    )
   }
 
   #[test]
@@ -113,55 +126,63 @@ mod tests {
   fn parse_delete_fails_when_key_is_missing(){
     assert_eq!(
       parse(&String::from(":delete  ")),
-      Err(String::from("Unknown command")))
+      Err(Error::ParseError)
+    )
   }
 
   #[test]
-  fn parse_delete_ignored_garbage(){
+  fn parse_delete_fails_with_appended_garbage(){
     assert_eq!(
       parse(&String::from(":delete foo garbagelfdsjlkf")),
-      Ok(Command::Delete(String::from("foo"))));
+      Err(Error::ParsingIncomplete)
+    )
   }
 
   #[test]
   fn parse_lookup_succeeds(){
     assert_eq!(
       parse(&String::from(":lookup foo")),
-      Ok(Command::Lookup(String::from("foo"))));
+      Ok(Command::Lookup(String::from("foo")))
+    )
   }
 
 #[test]
   fn parse_lookup_fails_when_key_is_missing(){
     assert_eq!(
       parse(&String::from(":lookup  ")),
-      Err(String::from("Unknown command")))
+      Err(Error::ParseError)
+    )
   }
 
   #[test]
-  fn parse_lookup_ignores_garbage(){
+  fn parse_lookup_fails_with_appended_garbage(){
     assert_eq!(
       parse(&String::from(":lookup foo garbagelfdsjlkf")),
-      Ok(Command::Lookup(String::from("foo"))));
+      Err(Error::ParsingIncomplete)
+    )
   }
 
   #[test]
   fn parse_list_keys_succeeds() {
-   assert_eq!(
+    assert_eq!(
       parse(&String::from(":list_keys")), 
-      Ok(Command::ListKeys))
+      Ok(Command::ListKeys)
+    )
   }
 
 #[test]
   fn parse_help_succeeds() {
-   assert_eq!(
+    assert_eq!(
       parse(&String::from(":help")), 
-      Ok(Command::Help))
+      Ok(Command::Help)
+    )
   }
 
   #[test]
   fn parse_quit_succeeds() {
     assert_eq!(
       parse(&String::from(":quit")),
-      Ok(Command::Quit))
+      Ok(Command::Quit)
+    )
   }
 }
